6ddd66ff5cd9b6e950ef209e73cc7fed0abc78bc
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-09-08 16:18:24 +0300

    Extended the ServiceError struct

--- src/services/errors.rs
+++ src/services/errors.rs
@@ -1,14 +1,36 @@
 use thiserror::Error;
 
 /// Generic error type used by service layer functions.
-#[derive(Debug, Error, PartialEq, Eq)]
+#[derive(Debug, Error)]
 pub enum ServiceError {
     /// The user is not authorized to perform the operation.
     #[error("unauthorized")]
     Unauthorized,
+
     /// Requested resource was not found.
     #[error("not found")]
     NotFound,
+
+    /// Requested resource was not found.
+    #[error("conflict")]
+    Conflict,
+
+    /// Persistence layer failures.
+    #[error("repository error: {0}")]
+    Repository(#[from] pushkind_common::repository::errors::RepositoryError),
+
+    /// ZmqSenderError
+    #[error("repository error: {0}")]
+    ZmqSender(#[from] pushkind_common::zmq::ZmqSenderError),
+
+    /// Form validation error.
+    #[error("form error: {0}")]
+    Form(String),
+
+    /// Problems with environment or configuration.
+    #[error("configuration error: {0}")]
+    Config(String),
+
     /// An unexpected internal error occurred.
     #[error("internal error")]
     Internal,
