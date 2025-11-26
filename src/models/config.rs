b502b072d1b3b3dd2bb06eca7bcdacd18287aaf7
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-11-26 23:28:58 +0300

    Use config instead of env

--- src/models/config.rs
+++ src/models/config.rs
@@ -1,5 +1,13 @@
-#[derive(Clone)]
+use serde::Deserialize;
+
+#[derive(Clone, Debug, Deserialize)]
 /// Basic configuration shared across handlers.
 pub struct ServerConfig {
     pub domain: String,
+    pub database_url: String,
+    pub address: String,
+    pub port: u16,
+    pub zmq_emailer_pub: String,
+    pub templates_dir: String,
+    pub secret: String,
 }
